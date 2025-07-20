d22904c1200f0b1d33c7d1b366f05e014bda4ab4
UnderlinkTrade <sebastian.1088@gmail.com>
2025-07-20 10:38:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Este archivo no se usa, pero permite que Railway detecte Python.")
+import subprocess
+
+print("⚙️ Entrenando o cargando modelos...")
+subprocess.run(["python", "src/modelCurren.py"], check=True)
+
+print("⚙️ Descargando data......")
+subprocess.run(["python", "src/data_loader.py"], check=True)
+
+print("🚀 Iniciando monitoreo en vivo...")
+subprocess.run(["python", "src/live_monitorCurren.py"], check=True)
